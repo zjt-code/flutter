1855679dd2ddaa14b2942450f60c6096fbc4fbe7
zjt-code <77527193+zjt-code@users.noreply.github.com>
2025-02-25 21:24:43 +0800

    add lexical_scope

--- dart_study/function.dart
+++ dart_study/function.dart
@@ -14,7 +14,7 @@ var _nobleGases = {
  bool isNolbe_b(int atomicNumber)=>_nobleGases[atomicNumber] !=null;
  
  // optional parameters
- 
+
 String say(String from, String msg,[String ?device])
 {
   String result = "$from says $msg";
@@ -25,9 +25,48 @@ String say(String from, String msg,[String ?device])
   return result;
 }
 
-void main()
-{
-  print("hello world\r\n");
 
-  print(say("fasfs","fsfsfs","my phone" ));
+// list and map
+
+void dostuff(
+          {List<int> list= const[1,3,2],
+          Map<String,String>gifts=const
+          {"first":"paper",
+          "second":'cotton',
+          'third':'leather'}})
+          {
+                print('list:  $list');
+                print('gifts: $gifts');
+          }
+// void main()
+// {
+//   print("hello world\r\n");
+
+//   print(say("fasfs","fsfsfs","my phone" ));
+
+//   dostuff();
+// }
+
+
+void printElement(int element)
+{
+  print(element);
+}
+
+var list=const[1,3,45,6];
+
+
+void main(List<String> arguments) {
+  print(arguments);
+
+  // assert(arguments.length == 2);
+  // assert(int.parse(arguments[0]) == 1);
+  // assert(arguments[1] == 'test');
+
+  list.forEach(print);
+  list.forEach((item){
+    print("${list.indexOf(item)}:$item");
+  });
+
+  list.forEach((item)=>print("${list.indexOf(item)}:$item"));
 }
---
+++ dart_study/lexical_scope.dart
@@ -0,0 +1,23 @@
+
+bool topLevel =true;
+
+
+void main()
+{
+ bool insideMain = false;
+void myFunction() {
+    var insideFunction = true;
+
+    void nestedFunction() {
+      var insideNestedFunction = true;
+
+      assert(topLevel);
+      assert(insideMain);
+      assert(insideFunction);
+      assert(insideNestedFunction);
+    }
+  }
+
+
+
+}
