3d59eba1ded0ceb1886a5c69ce46495df09789ab
zjt-code <77527193+zjt-code@users.noreply.github.com>
2025-03-23 22:00:32 +0800

    basical_form

---
+++ flutter_basicalwidget_6/android/app/src/main/kotlin/com/example/flutter_basicalwidget_6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_basicalwidget_6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_basicalwidget_6/lib/main.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // // Column has various properties to control how it sizes itself and
+          // // how it positions its children. Here we use mainAxisAlignment to
+          // // center the children vertically; the main axis here is the vertical
+          // // axis because Columns are vertical (the cross axis would be
+          // // horizontal).
+          // //
+          // // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // // action in the IDE, or press "p" in the console), to see the
+          // // wireframe for each widget.
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // const Text('You have pushed the button this many times:'),
+            // Text(
+            //   '$_counter',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
+             FormTestRoute(),
+          ],
+         
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class FormTestRoute extends StatefulWidget {
+  @override
+  _FormTestRouteState createState() => _FormTestRouteState();
+}
+
+class _FormTestRouteState extends State<FormTestRoute> {
+  TextEditingController _unameController = TextEditingController();
+  TextEditingController _pwdController = TextEditingController();
+  GlobalKey _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey, //设置globalKey，用于后面获取FormState
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            autofocus: true,
+            controller: _unameController,
+            decoration: InputDecoration(
+              labelText: "用户名",
+              hintText: "用户名或邮箱",
+              icon: Icon(Icons.person),
+            ),
+            // 校验用户名
+            validator: (v) {
+              return v!.trim().isNotEmpty ? null : "用户名不能为空";
+            },
+          ),
+          TextFormField(
+            controller: _pwdController,
+            decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "您的登录密码",
+              icon: Icon(Icons.lock),
+            ),
+            obscureText: true,
+            //校验密码
+            validator: (v) {
+              return v!.trim().length > 5 ? null : "密码不能少于6位";
+            },
+          ),
+          // 登录按钮
+          Padding(
+            padding: const EdgeInsets.only(top: 0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ElevatedButton(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text("登录"),
+                    ),
+                    onPressed: () {
+                      // 通过_formKey.currentState 获取FormState后，
+                      // 调用validate()方法校验用户名密码是否合法，校验
+                      // 通过后再提交数据。
+                      if ((_formKey.currentState as FormState).validate()) {
+                        //验证通过提交数据
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
