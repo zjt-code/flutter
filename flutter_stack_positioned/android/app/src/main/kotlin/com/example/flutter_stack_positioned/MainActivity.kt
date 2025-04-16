c34047c6bb3127baff526dc3669bcdea99349445
zjt-code <77527193+zjt-code@users.noreply.github.com>
2025-04-16 13:17:46 +0800

    add flutter stack and positioned

---
+++ flutter_stack_positioned/android/app/src/main/kotlin/com/example/flutter_stack_positioned/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_stack_positioned
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_wrap_flow/android/app/src/main/kotlin/com/example/flutter_wrap_flow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_wrap_flow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
