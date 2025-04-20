c8fc84202a3466be14835de46fffc2113dcdc864
zjt-code <77527193+zjt-code@users.noreply.github.com>
2025-04-20 22:26:08 +0800

    flutter_align

---
+++ flutter_align/android/app/src/main/kotlin/com/example/flutter_align/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_align
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_layoutbuild/android/app/src/main/kotlin/com/example/flutter_layoutbuild/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_layoutbuild
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
